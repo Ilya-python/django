4e22271f504881d3b993c36f52a9c00729edcc6d
Ilya-python <nwar80765@gmail.com>
2022-11-03 19:48:47 +0300

    прпр

--- games/views.py
+++ games/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from .models import Games
+from .models import Games, Category
 # Create your views here.
 
 def index(request):
+    """
+    отвечает за гл страницу
+    """
     content = Games.objects.filter(is_pub=True)
     context = {'games':content, 'mark':1}
     return render(request, 'games/games.html', context)
 
 def get_game(request, slug):
+    """
+    отвечает за страницу содной игрой
+    :param slug Уникальный слаг игры
+    """
     game = Games.objects.get(game_slug=slug)
     return render(request, 'games/sing_game.html', {'game':game})
 
